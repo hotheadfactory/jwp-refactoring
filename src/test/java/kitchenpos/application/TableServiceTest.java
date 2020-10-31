ff0e2112c276e3ef567ec227def21140bfaf7ef9
chws <rebeccachoosir@gmail.com>
2020-10-31 12:05:48 +0900

    test: 테이블 생성 테스트 추가

---
+++ src/test/java/kitchenpos/application/TableServiceTest.java
@@ -0,0 +1,44 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import kitchenpos.dao.OrderDao;
+import kitchenpos.dao.OrderTableDao;
+import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TableServiceTest {
+
+    @Mock
+    private OrderDao orderDao;
+
+    @Mock
+    private OrderTableDao orderTableDao;
+
+    @InjectMocks
+    private TableService tableService;
+
+    @BeforeEach
+    void setUp() {
+        tableService = new TableService(orderDao, orderTableDao);
+    }
+
+    @Test
+    @DisplayName("테이블을 생성하는 테스트")
+    void create() {
+        OrderTable orderTable = new OrderTable();
+
+        given(orderTableDao.save(any())).willReturn(orderTable);
+
+        assertThat(tableService.create(orderTable)).isEqualTo(orderTable);
+    }
+}
